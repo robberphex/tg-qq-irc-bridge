3a9e2ed7edcaf0091175edeaaa7688e39397097e
Robert Lu <robberphex@gmail.com>
2017-12-12 17:10:46 +0800

    屏蔽表情和gif图

--- bots/qq_bot.py
+++ bots/qq_bot.py
@@ -1,5 +1,6 @@
 import asyncio
 import html
+import re
 
 import aiohttp
 from aiohttp import ClientWebSocketResponse
@@ -30,21 +31,37 @@ def create_qq_bot(base_uri, group_id, blacklist=None, password=None, loop=None):
                         and 'group_id' in message and message['group_id'] == group_id:
                     if message['user_id'] in blacklist:
                         continue
-                    call = {
-                        "action": "get_group_member_info",
-                        "params": {
-                            "group_id": group_id,
-                            "user_id": message['user_id'],
-                            "no_cache": "true"
-                        }
-                    }
-                    await api_ws.send_json(call)
-                    resp = await api_ws.receive_json()
-                    author = resp['data']['card']
-                    if not author:
-                        author = resp['data']['nickname']
-                    final_msg = '[{author}] {msg}'.format(author=author, msg=html.unescape(message['message']))
-                    await receive_queue.put(final_msg)
+                    text_message = message['message']
+                    text_message = re.sub(
+                        r"\[CQ:face,id=\d+\]",
+                        " ",
+                        text_message
+                    )
+                    text_message = re.sub(
+                        r"\[CQ:image,file=\w+\.gif,url=[^\]]+\]",
+                        " ",
+                        text_message
+                    )
+                    text_message = text_message.strip()
+                    if text_message:
+                        await receive_msg_content(text_message, message['user_id'], api_ws)
+
+    async def receive_msg_content(text_messages, user_id, api_ws):
+        for text_message in text_messages.splitlines():
+            call = {
+                "action": "get_group_member_info",
+                "params": {
+                    "group_id": group_id,
+                    "user_id": user_id
+                }
+            }
+            await api_ws.send_json(call)
+            resp = await api_ws.receive_json()
+            author = resp['data']['card']
+            if not author:
+                author = resp['data']['nickname']
+            final_msg = '[{author}] {msg}'.format(author=author, msg=html.unescape(text_message))
+            await receive_queue.put(final_msg)
 
     async def send_msg(base_uri):
         async with session.ws_connect(base_uri + '/api/') as ws:  # type: ClientWebSocketResponse
